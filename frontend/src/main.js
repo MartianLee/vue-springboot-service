b95163ceface6889195372e84b0fdfe8ad58be22
MartianLee <martionlee@gmail.com>
2019-05-29 23:33:52 +0900

    add material-icon in package

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,5 +1,8 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+// include CSS
+import 'material-design-icons-iconfont/dist/material-design-icons.css'
+
 import Vue from 'vue'
 import Vuex from 'vuex'
 import App from './App'
