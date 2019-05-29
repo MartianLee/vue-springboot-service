5937d1e113f605a4f06763b0092b1aae533e82ca
MartianLee <martionlee@gmail.com>
2019-05-29 23:34:41 +0900

    modify WriteDiary page with Vuetify components

--- frontend/src/components/Diary/WriteDiary.vue
+++ frontend/src/components/Diary/WriteDiary.vue
@@ -1,47 +1,65 @@
 <template>
   <div class="WriteDiary">
     <h1>Write Diary</h1>
-    <div class="diary">
+    <div class="container">
+      <v-btn v-on:click="onClickBack">
+        back
+      </v-btn>
+      <div class="diary">
         <form class="diary-form" v-on:submit.prevent="onCreateDiary" method="POST">
             <input type="hidden" th:name="${_csrf.parameterName}" th:value="${_csrf.token}" />
-            <div class="row">
-                <div class="input-field col s12">
-                  <label for="title">Title</label>
-                  <input id="title" name="title" type="title" class="validate" v-model="title"/>
-                </div>
-            </div>
-            <div class="row">
-                <div class="input-field col s12">
-                  <label for="content">Content</label>
-                  <textarea id="content" name="content" rows="6" class="validate" v-model="content"/>
-                </div>
-            </div>
-            <div class="row">
-              <select v-model="selected_league" v-on:change="onChangeLeague()">
-                <option v-for="league in league_list" v-bind:key="league.id" v-bind:value="league.id">
-                  {{ league.name }}
-                </option>
-              </select>
-              <span>선택함: {{ selected_league }}</span>
-            </div>
-            <div class="row">
-              <select v-model="selected_team" v-on:change="onChangeTeam()">
-                <option v-for="team in team_list" v-bind:key="team.id" v-bind:value="team.id">
-                  {{ team.name }}
-                </option>
-              </select>
-              <span>선택함: {{ selected_team }}</span>
-            </div>
-            <div class="row">
-              <select v-model="selected_match">
-                <option v-for="match in match_list" v-bind:key="match.id" v-bind:value="match.id">
-                  {{ match.homeTeam.name }} vs {{ match.awayTeam.name }} at {{ match.matchDate }}
-                </option>
-              </select>
-              <span>선택함: {{ selected_match }}</span>
-            </div>
-            <input class="diary-btn waves-effect waves-light btn" type="submit" value="글쓰기" />
+            <v-text-field
+              v-model="title"
+              :counter="100"
+              label="Title"
+              required
+            ></v-text-field>
+            <v-textarea
+              v-model="content"
+              :counter="1000"
+              label="Content"
+              required
+            ></v-textarea>
+            <v-layout row wrap align-center>
+              <v-flex sm6 xs12>
+                <v-subheader>League</v-subheader>
+              </v-flex>
+              <v-flex sm6 xs12>
+                <v-select :items="league_list"
+                  v-on:change="onChangeLeague()"
+                  v-model="selected_league"
+                  item-text="name"
+                  item-value="id"
+                  outline>
+                </v-select>
+              </v-flex>
+              <v-flex sm6 xs12>
+                <v-subheader>Team</v-subheader>
+              </v-flex>
+              <v-flex sm6 xs12>
+                <v-select :items="team_list"
+                  v-on:change="onChangeTeam()"
+                  v-model="selected_team"
+                  item-text="name"
+                  item-value="id"
+                  outline>
+                </v-select>
+              </v-flex>
+              <v-flex sm6 xs12>
+                <v-subheader>Match</v-subheader>
+              </v-flex>
+              <v-flex sm6 xs12>
+                <v-select :items="match_list"
+                :item-text="matchTitle"
+                v-model="selected_match"
+                item-value="id"
+                outline>
+                </v-select>
+              </v-flex>
+            </v-layout>
+            <v-btn type="submit">write</v-btn>
         </form>
+      </div>
     </div>
   </div>
 </template>
@@ -75,10 +93,39 @@ export default {
           console.log('failed')
         }).then(resp => {
         this.league_list = resp.data.response
+      }).then(() => {
+        console.log(this.$route.params)
+        if (this.$route.params.id) {
+          this.$http.get('/api/match/' + this.$route.params.id,
+            {
+              headers: {
+                Authorization: 'Bearer ' + token, // the token is a variable which holds the token
+                'Content-Type': 'application/json'
+              }
+            }, () => {
+              console.log('failed')
+            }).then(resp => {
+            console.log('asdf')
+            this.selected_match = resp.data.response.id
+            this.selected_league = resp.data.response.league.id
+            this.selected_team = resp.data.response.homeTeam.id
+            this.matchId = resp.data.response.id
+            this.onChangeLeague()
+            this.onChangeTeam()
+          })
+        }
       })
     }
   },
   methods: {
+    matchTitle: function (item) {
+      if (item) {
+        return `${item.homeTeam.name} vs ${item.awayTeam.name}`
+      }
+    },
+    onClickBack: function () {
+      this.$router.go(-1)
+    },
     onChangeLeague: function () {
       let token = window.$cookies.get('FootballDiary')
       if (token) {
